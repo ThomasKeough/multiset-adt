cb89049517ae195e1f62771c15deda8e704cdf34
Thomas <thomas.keough@mail.utoronto.ca>
2023-09-18 17:01:41 -0400

    made tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Tree {
+    private Integer root;
+    private ArrayList<Tree> subtrees;
+
+    public Tree(Integer root, ArrayList<Tree>) {
+        this.root = root;
+
+
+    }
 }
